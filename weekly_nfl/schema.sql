2f5043d0250c795216e352ba721ceeee3679db50
cjreside <connorjreside@gmail.com>
2020-11-12 14:42:21 -0500

    update files

--- weekly_nfl/schema.sql
+++ weekly_nfl/schema.sql
@@ -1,59 +1,7 @@
-DROP TABLE IF EXISTS all_events_all_weeks, all_plays, weekly_nfl_games;
-
--- Create Tables
-CREATE TABLE all_events_all_weeks (
-    time VARCHAR,
-    x FLOAT,
-    y FLOAT,
-    s INT,
-    a INT,
-    dis INT,
-	o INT,
-	dir INT,
-	event TEXT,
-	nflId INT,
-	displayName TEXT,
-	jerseyNumber INT,
-	position TEXT,
-	frameId INT,
-	team TEXT,
-	gameId INT,
-	playId INT,
-	playDirection TEXT,
-	route TEXT
-);
-
-CREATE TABLE all_plays (
-    gameId INT,
-	playId INT,
-	playDescription VARCHAR,
-	quarter INT,
-	down INT,
-	yardsToGo INT,
-	possessionTeam TEXT,
-	playType VARCHAR,
-	yardlineSide TEXT,
-	yardlineNumber INT,
-	offenseFormation TEXT,
-	personnelO VARCHAR,
-	defendersinTheBox INT,
-	numberofPassRushers INT,
-	personnelID VARCHAR,
-	typeDropback VARCHAR,
-	preSnapVisitorScore INT,
-	preSnapHomeScore INT,
-	gameClock VARCHAR,
-	absoluteYardlineNumber INT,
-	penaltyCodes VARCHAR,
-	penaltyJerseyNumber VARCHAR,
-	passResult TEXT,
-	offensePlayResult INT,
-	playResult INT,
-	epa INT
-);
+DROP TABLE IF EXISTS weekly_nfl_games, all_events_all_weeks, all_plays;
 
 CREATE TABLE weekly_nfl_games (
-	gameId INT,
+	gameId INT PRIMARY KEY,
 	WEEK VARCHAR,
 	WINNER TEXT,
 	LOSER TEXT,
